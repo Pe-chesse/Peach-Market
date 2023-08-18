999132beee9fa4c4ee104784c022c75de57e0e5d
kimyeoju <yeojoo0031@gmail.com>
2023-08-18 14:54:54 +0900

    feat(post): Add Model and Serializer Complete

--- post/models.py
+++ post/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth import User
 
-# Create your models here.
+
+# Create your models here.s
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    body = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.BooleanField(default=True)
+    img_url = models.ImageField(null=True, upload_to="", blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    body = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.body
---
+++ post/serializer.py
@@ -0,0 +1,13 @@
+from .models import Post, Comment
+from rest_framework import serializers
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+        
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
