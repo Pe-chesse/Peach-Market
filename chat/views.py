ed53a6199af2895dd4e0f70ab148b1417b76e3aa
Deeklming <deeklming6850@gmail.com>
2023-08-21 15:42:49 +0900

    feat(chat): Added new features and changed setup

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+# from rest_framework.response import Response
+# from rest_framework.views import APIView
+from django.views import View
 
-# Create your views here.
+
+def chat_index(req):
+    return render(req, "chat/chat_index.html")
+
+def chat_room(req, room_name):
+    return render(req, "chat/chat_room.html", {"room_name": room_name})
+
+# class ChatIndex(View):
+#     def get(self, req):
+#         return render(req, "chat/chat_index.html")
+
+# class ChatRoom(View):
+#     def get(self, req, room_name):
+#         return render(req, "chat/chat_room.html", {"room_name": room_name})
--- peach_market/asgi.py
+++ peach_market/asgi.py
@@ -8,9 +8,21 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-
+import chat.routing
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'peach_market.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+        ),
+    }
+)
