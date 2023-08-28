8c81278cb5d2126009aa203a1f2eccafb33894aa
JayG-5 <jg55552758@gmail.com>
2023-08-28 21:17:36 +0900

    fixed(deploy): entrypoint and compose

--- config/docker/entrypoint.prod.sh
+++ config/docker/entrypoint.prod.sh
@@ -1,6 +1,6 @@
 
 echo "Collecting static files..."
-python manage.py collectstatic --no-input --clear
+python manage.py collectstatic --clear
 
 # Apply database migrations
 echo "Applying database migrations..."
