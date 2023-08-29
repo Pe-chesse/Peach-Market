6d27846310cd9b740de4791c1dddcefdc859213e
JayG-5 <jg55552758@gmail.com>
2023-08-29 11:16:04 +0900

    hotfix(static): uploads staticfiles to S3 bucket

--- peach_market/settings.py
+++ peach_market/settings.py
@@ -192,4 +192,4 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
