7245d32dacde1667bb06660cac4de6dcd8d6404b
JayG-5 <jg55552758@gmail.com>
2023-08-29 15:36:31 +0900

    hotfix(socket): asgi.py

--- peach_market/asgi.py
+++ peach_market/asgi.py
@@ -7,6 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
+
 import os
 
 from channels.auth import AuthMiddlewareStack
@@ -14,7 +15,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import chat.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "peach_market.settings")
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
