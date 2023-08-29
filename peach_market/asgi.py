41be253fd593aed445754e58747885c51f2c66bc
JayG-5 <jg55552758@gmail.com>
2023-08-29 15:42:08 +0900

    hotfix(socket): asgi.py

--- peach_market/asgi.py
+++ peach_market/asgi.py
@@ -10,18 +10,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "peach_market.settings")
 
-application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
-    ),
-})
+application = get_asgi_application()
