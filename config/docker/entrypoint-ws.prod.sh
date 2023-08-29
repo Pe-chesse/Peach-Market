655b4d08646eeb80ccc4f55f071b09891e4795ff
JayG-5 <jg55552758@gmail.com>
2023-08-29 21:01:08 +0900

    hotfix(docker): fixed entrypoint

--- config/docker/entrypoint-ws.prod.sh
+++ config/docker/entrypoint-ws.prod.sh
@@ -7,4 +7,5 @@ echo "Applying database migrations..."
 python manage.py migrate
 
 echo "Starting uvicorn..."
+export DJANGO_SETTINGS_MODULE=peach_market.settings
 gunicorn -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8001 peach_market.asgi:application
