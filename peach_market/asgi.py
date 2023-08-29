1a57ad7c600725c9a718fc803e93fbe04f8cffef
JayG-5 <jg55552758@gmail.com>
2023-08-29 22:29:31 +0900

    hotfix(ws): fixed asgi

--- peach_market/asgi.py
+++ peach_market/asgi.py
@@ -6,14 +6,15 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from django.urls import path
-from chat.consumers import ChatConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'peach_market.settings')
 
+
 django_asgi_app = get_asgi_application()
 
+from chat.consumers import ChatConsumer
 application = ProtocolTypeRouter({
-    # "http": django_asgi_app,
+    "http": django_asgi_app,
     "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter([
