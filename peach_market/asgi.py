c54dcd790d037bbbd38d88fb2909e015b168f183
JayG-5 <jg55552758@gmail.com>
2023-08-29 16:29:36 +0900

    hotfix(socket): routing.py

--- peach_market/asgi.py
+++ peach_market/asgi.py
@@ -1,17 +1,16 @@
-"""
-ASGI config for peach_market project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from ..chat import routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "peach_market.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    ),
+})
