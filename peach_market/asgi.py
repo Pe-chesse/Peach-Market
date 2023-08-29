4e5611f79cfe213029eaaa714461bab661de7b7f
JayG-5 <jg55552758@gmail.com>
2023-08-29 17:26:04 +0900

    hotfix(socket): asgi.py

--- peach_market/asgi.py
+++ peach_market/asgi.py
@@ -2,7 +2,7 @@ import os
 import django
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'peach_market.settings')
 
 django.setup()
 
