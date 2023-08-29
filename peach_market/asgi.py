fa49a4f004b3a0b46bc2866209d1a3d155495015
JayG-5 <jg55552758@gmail.com>
2023-08-29 21:46:49 +0900

    hotfix(socket): asgi.py

--- peach_market/asgi.py
+++ peach_market/asgi.py
@@ -1,20 +1,26 @@
+
 import os
-import django
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
+
 from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
 from django.urls import path
 from chat.consumers import ChatConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'peach_market.settings')
 
-django.setup()
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter([
-            path("ws/v1/chat/", ChatConsumer.as_asgi()),
-        ])
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter([
+                path("ws/v1/chat/", ChatConsumer.as_asgi()),
+            ])
+        )
     ),
 })
+
+
