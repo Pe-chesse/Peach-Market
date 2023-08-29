1611ede2383c4974d0962d8e696a4842fa2b961c
JayG-5 <jg55552758@gmail.com>
2023-08-29 15:47:17 +0900

    hotfix(socket): asgi.py

--- config/docker/entrypoint-ws.prod.sh
+++ config/docker/entrypoint-ws.prod.sh
@@ -1,3 +1,12 @@
+
+echo "Collecting static files..."
+python manage.py collectstatic --no-input
+
+# Apply database migrations
+echo "Applying database migrations..."
+python manage.py migrate
+
+
 echo "Starting Gunicorn..."
 gunicorn peach_market.asgi:application -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8001
 
