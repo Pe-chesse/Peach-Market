0baa1516c950914187ff02978a94bdebffd25d39
JayG-5 <jg55552758@gmail.com>
2023-08-29 17:24:15 +0900

    hotfix(socket): routing.py

--- peach_market/asgi.py
+++ peach_market/asgi.py
@@ -1,22 +1,9 @@
 import os
 import django
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from chat import routing
 
-try:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "peach_market.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
-    django.setup()
+django.setup()
 
-    application = ProtocolTypeRouter({
-        "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(
-            URLRouter(
-                routing.websocket_urlpatterns
-            )
-        ),
-    })
-except Exception as e:
-    print(e)
+application = get_asgi_application()
---
+++ peach_market/routing.py
@@ -0,0 +1,15 @@
+# routing.py
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.urls import path
+
+from chat.consumers import ChatConsumer
+
+application = ProtocolTypeRouter({
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            path("ws/v1/chat/", ChatConsumer.as_asgi()),
+        ])
+    ),
+})
