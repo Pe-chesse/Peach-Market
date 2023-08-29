5cb91894b794dcda03255f858edfa59a8e8c7715
JayG-5 <jg55552758@gmail.com>
2023-08-29 17:13:25 +0900

    hotfix(docker): fixed entrypoint

--- config/docker/entrypoint-ws.prod.sh
+++ config/docker/entrypoint-ws.prod.sh
@@ -6,9 +6,5 @@ python manage.py collectstatic --no-input
 echo "Applying database migrations..."
 python manage.py migrate
 
-
-echo "Starting Gunicorn..."
-gunicorn peach_market.asgi:application -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8001
-
 echo "Starting Uvicorn..."
 uvicorn --reload peach_market.asgi:application --host 0.0.0.0 --port 8001
