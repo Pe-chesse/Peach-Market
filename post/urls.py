76f83423199de0264a2d6b3169b08ce8236e34f4
JayG-5 <jg55552758@gmail.com>
2023-08-31 12:24:39 +0900

    hotfix(post): post.urls.py

--- post/urls.py
+++ post/urls.py
@@ -4,7 +4,6 @@ from django.conf.urls.static import static
 from django.views.static import serve
 from django.conf import settings
 from . import views
-from .views import PostSearchAPIView
 
 urlpatterns =[
     # <Post - 게시글>
